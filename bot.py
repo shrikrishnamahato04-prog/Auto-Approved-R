bd4db8cbfd06a4824da3fa8dc424735983ac04bb
shrikrishnamahato04-prog <shrikrishnamahato04@gmail.com>
2025-12-24 04:43:15 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,41 +1,21 @@
 # MIT License
-
 # Copyright (c) 2022 Muhammed
-
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
-
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
-
-# Telegram Link : https://telegram.dog/Mo_Tech_Group
-# Repo Link : https://github.com/PR0FESS0R-99/Auto-Approved-Bot
-# License Link : https://github.com/PR0FESS0R-99/Auto-Approved-Bot/blob/Auto-Approved-Bot/LICENSE
 
 from os import environ
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message, User, ChatJoinRequest
 
+# Bot Client Setup
+# session_string line add karne se timing error (msg_id) fix ho jayega
 pr0fess0r_99=Client(
     "Auto Approved Bot",
     bot_token = environ["BOT_TOKEN"],
     api_id = int(environ["API_ID"]),
-    api_hash = environ["API_HASH"]
+    api_hash = environ["API_HASH"],
+    session_string = environ.get("SESSION_STRING") # <-- Ye line maine add ki hai
 )
 
-CHAT_ID = [int(pr0fess0r_99) for pr0fess0r_99 in environ.get("CHAT_ID", None).split()]
+CHAT_ID = [int(pr0fess0r_99) for pr0fess0r_99 in environ.get("CHAT_ID", "").split()]
 TEXT = environ.get("APPROVED_WELCOME_TEXT", "Hello {mention}\nWelcome To {title}\n\nYour Auto Approved")
 APPROVED = environ.get("APPROVED_WELCOME", "on").lower()
 
@@ -48,13 +28,12 @@ async def start(client: pr0fess0r_99, message: Message):
 
 @pr0fess0r_99.on_chat_join_request((filters.group | filters.channel) & filters.chat(CHAT_ID) if CHAT_ID else (filters.group | filters.channel))
 async def autoapprove(client: pr0fess0r_99, message: ChatJoinRequest):
-    chat=message.chat # Chat
-    user=message.from_user # User
-    print(f"{user.first_name} Joined 🤝") # Logs
+    chat=message.chat 
+    user=message.from_user 
+    print(f"{user.first_name} Joined 🤝") 
     await client.approve_chat_join_request(chat_id=chat.id, user_id=user.id)
     if APPROVED == "on":
         await client.send_message(chat_id=chat.id, text=TEXT.format(mention=user.mention, title=chat.title))
-    #   print("Welcome....")
 
-print("Auto Approved Bot")
+print("Auto Approved Bot is Starting...")
 pr0fess0r_99.run()
